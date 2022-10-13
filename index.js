c24f34f753d1611c40d185336b324098fca30077
Luna <luna.devacc@gmail.com>
2022-10-13 18:53:51 +0900

    refact: 외부 라이브러리 종속성 제거

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
 #!/usr/bin/env node
-const fs = require('fs');
-const clc = require('cli-color');
+const fs = require("fs");
 
-(()=>{
-    const root = process.argv[2]
-    if(root){
-        console.log('\x1b[32m%s\x1b[0m',"root : " + root);
-        fs.mkdirSync(`./${root}/domain/repository`, {recursive: true})
-        fs.mkdirSync(`./${root}/interface-adapter/dto`, {recursive: true})
-        fs.mkdirSync(`./${root}/use-case`, {recursive: true})
-        fs.mkdirSync(`./${root}/infra/entity`, {recursive: true})
-    }else {
-        console.error(clc.red('Error, usage: $mkdirca {root}'));
-    }
+(() => {
+  const root = process.argv[2];
+  if (root) {
+    console.log("\x1b[32m%s\x1b[0m", "root : " + root);
+    fs.mkdirSync(`${root}/domain/repository`, { recursive: true });
+    fs.mkdirSync(`${root}/interface-adapter/dto`, { recursive: true });
+    fs.mkdirSync(`${root}/use-case`, { recursive: true });
+    fs.mkdirSync(`${root}/infra/entity`, { recursive: true });
+  } else {
+    console.error("\x1b[31m%s\x1b[0m", "Error, usage: $mkdirca {root}");
+  }
 })();
